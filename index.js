8a27bd6670e376feb7f79b2355c17d41d3df2fef
noor-003 <165676935+noor-003@users.noreply.github.com>
2024-04-01 20:44:45 +0500

    Calculator Pushed

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+//it tells that it is not an oridinary file but it tells that cide will be executable on terminal.
+//it will be available to publish and can be executable by npx tool
+//sha#bang!
+import inquirer from "inquirer";
+// here user will wait 
+// [] Array
+// {} Object
+// message is key
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of these operators to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
+//Conditional Statements
+const calculate = (num1, operator, num2) => {
+    switch (operator) {
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '*': return num1 * num2;
+        case '/': return num1 / num2;
+        default: throw new Error('Invalid operator');
+    }
+};
+// Conditional Statements
+if (answer.operator === "Addition") {
+    // "Your Value is: " 
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalid Input......");
+}
